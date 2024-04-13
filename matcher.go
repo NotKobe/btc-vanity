2b85de123608435fdb9339521f512eaeeb567491
NotKobe <al.10549329@gmail.com>
2024-04-13 12:17:17 -0500

    use all

--- matcher.go
+++ matcher.go
@@ -4,7 +4,7 @@ import "strings"
 
 // isMatch checks if the btc wallet matches the prefix of pattern
 func isMatch(pattern string, addr string) bool {
-	addr = strings.ToUpper(addr[1 : len(addr)])
+	addr = strings.ToUpper(addr[1 : ])
 	pattern = strings.ToUpper(pattern)
 	return strings.HasSuffix(addr, pattern)
 }
